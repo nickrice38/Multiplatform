e6edcf34912b9979bdd63396afeaf663a484f175
Nick Rice <enprice383@gmail.com>
2022-01-31 17:45:09 +0000

    Initial Commit

---
+++ Shared/MultiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplatformApp.swift
+//  Shared
+//
+//  Created by Nick Rice on 31/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
