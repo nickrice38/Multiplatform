1b0104f8ad32c58231a8f82e214b0cca822558dc
Nick Rice <enprice383@gmail.com>
2022-02-02 20:24:59 +0000

    - Added more matchedGeometryEffect to work in LazyGrids - Fixed zIndex issues - Disabled cards mid-animation - Cards using sample data

--- CoursesView.swift
+++ CoursesView.swift
@@ -10,32 +10,71 @@ import SwiftUI
 struct CoursesView: View {
     @State var show = false
     @Namespace var namespace
+    @State var selectedItem: Course? = nil
+    @State var isDisabled = false
     
     var body: some View {
         ZStack {
-            CourseItem()
-                .matchedGeometryEffect(id: "Card", in: namespace, isSource: !show) // which card is the original
-                .frame(width: 335, height: 250)
-            if show {
-                ScrollView {
-                    CourseItem()
-                        .matchedGeometryEffect(id: "Card", in: namespace)
-                        .frame(height: 300)
+            ScrollView {
+                LazyVGrid(
+                    columns: [GridItem(.adaptive(minimum: 160), spacing: 16)],
+                    spacing: 16
+                ) {
+                    ForEach(courses) { item in
+                        VStack {
+                            CourseItem(course: item)
+                                .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show) // which card is the original
+                                .frame(height: 200)
+                            .onTapGesture {
+                                withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) {
+                                    show.toggle()
+                                    selectedItem = item
+                                    isDisabled = true
+                                }
+                            }
+                        .disabled(isDisabled)
+                        }
+                        .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show) // this container is going to have its own id
+                    }
+                }
+                .padding(16)
+                .frame(maxWidth: .infinity)
+            }
+            
+            .zIndex(1)
+            if selectedItem != nil {
+                ZStack(alignment: .topTrailing) {
                     VStack {
-                        ForEach(0 ..< 20) { item in
-                            CourseRow()
+                        ScrollView {
+                            CourseItem(course: selectedItem!)
+                                .matchedGeometryEffect(id: selectedItem!.id, in: namespace)
+                                .frame(height: 300)
+                            VStack {
+                                ForEach(0 ..< 20) { item in
+                                    CourseRow()
+                                }
+                            }
+                            .padding()
                         }
                     }
-                    .padding()
+                    .background(Color("Background 1"))
+                    .clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
+                    .matchedGeometryEffect(id: "container\(selectedItem!.id)", in: namespace)
+                    .edgesIgnoringSafeArea(.all)
+                    
+                    CloseButton()
+                        .padding(.trailing, 16)
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                show.toggle()
+                                selectedItem = nil
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                    isDisabled = false
+                                }
+                        }
+                    }
                 }
-                
-                .transition(.opacity)
-                .edgesIgnoringSafeArea(.all)
-            }
-        }
-        .onTapGesture {
-            withAnimation(.spring()) {
-                show.toggle()
+                .zIndex(2)
             }
         }
     }
--- Shared/MultiplatformApp.swift
+++ Shared/MultiplatformApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MultiplatformApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CoursesView()
         }
     }
 }
